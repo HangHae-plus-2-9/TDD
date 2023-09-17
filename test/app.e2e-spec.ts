bc7ae1c61af3fd5f6bd112231fa2d35a03808f3c
HC-kang <weston0713@gmail.com>
2023-09-17 15:58:14 +0900

    Remove: console.log

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,17 +7,13 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    console.log('Setting up the test module...');
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-    console.log('Test module compiled.');
 
     app = moduleFixture.createNestApplication();
-    console.log('Nest application created.');
 
     await app.init();
-    console.log('Nest application initialized.');
   });
 
   it('/ (GET)', () => {
