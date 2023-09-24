5946efb4b71cc074687a21a51a854756c9974dae
HC-kang <weston0713@gmail.com>
2023-09-24 17:20:00 +0900

    Feat: add basic common configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,5 @@
+export * from './helpers';
+export * from './winston.config';
 export * from './database.config';
+export * from './swagger.config';
+export * from './api-auth.config';
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptors';
+import { WinstonLogger, setupApiAuth, setupSwagger } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonLogger,
+  });
 
   app.setGlobalPrefix('api');
   app.enableVersioning();
   app.enableCors({ origin: '*' });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  setupApiAuth(app);
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
