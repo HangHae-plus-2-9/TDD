c60db0c247b621fae719f44eaec2afb64b926ed3
LAPTOP-EIABQG0H\tulip <tasha@xquant-ai.com>
2023-10-04 21:58:29 +0900

    commenting

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,21 +1,21 @@
-import { IsInt, IsString, IsOptional, Min, IsNumber } from 'class-validator';
+// import { IsString } from 'class-validator';
 
 export class CreateProductDto {
 
-  @IsInt()
-  sellerId: number; // 사용자는 판매자의 ID만 제공합니다. 연관된 엔터티는 서비스 레벨에서 처리됩니다.
+  // @IsInt()
+  // sellerId: number; // 사용자는 판매자의 ID만 제공합니다. 연관된 엔터티는 서비스 레벨에서 처리됩니다.
 
-  @IsString()
-  name: string;
+  // @IsString()
+  // name: string;
 
-  @IsString()
-  price: string;
+  // @IsString()
+  // price: string;
 
-  @IsNumber()
-  @Min(0)
-  quantity: number;
+  // @IsNumber()
+  // @Min(0)
+  // quantity: number;
 
-  @IsString()
-  @IsOptional() // 선택적 필드입니다.
-  description?: string;
+  // @IsString()
+  // @IsOptional() // 선택적 필드입니다.
+  // description?: string;
 }
--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,28 +1,28 @@
 import { CommonColumns } from '@/common/entities/common-columns';
 // import { UserEntity } from '@/modules/users/entities/user.entity';
-import { Column, PrimaryGeneratedColumn, ManyToMany, JoinTable, ManyToOne, JoinColumn, Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'products' })
 export class ProductEntity extends CommonColumns {
   
-  @PrimaryGeneratedColumn()
-  id: number;
+  // @PrimaryGeneratedColumn()
+  // id: number;
 
-  // @ManyToOne(() => UserEntity, seller => seller.products)
-  // @JoinColumn({ name: 'seller_id' })  // This sets up the foreign key
-  // seller: UserEntity;
+  // // @ManyToOne(() => UserEntity, seller => seller.products)
+  // // @JoinColumn({ name: 'seller_id' })  // This sets up the foreign key
+  // // seller: UserEntity;
 
   @Column()
   name: string;
 
-  @Column()
-  price: string;
+  // @Column()
+  // price: string;
 
-  @Column()
-  quantity: number;
+  // @Column()
+  // quantity: number;
 
-  @Column({ nullable: true })
-  description?: string;
+  // @Column({ nullable: true })
+  // description?: string;
 
 
 }
