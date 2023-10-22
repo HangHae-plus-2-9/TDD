0523039a3f10bd13bf794af955bcdc5e9e0672cd
HC-kang <weston0713@gmail.com>
2023-10-22 14:10:40 +0900

    Test: make tests pass for payment controller

--- src/modules/payments/payments.controller.spec.ts
+++ src/modules/payments/payments.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+  let mockService: Partial<PaymentsService>;
 
   beforeEach(async () => {
+    mockService = {};
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentsController],
+      providers: [
+        {
+          provide: PaymentsService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PaymentsController>(PaymentsController);
